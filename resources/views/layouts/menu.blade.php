9b5a246fa0f9d3e180414d72c1a23864985bba8b
= <=>
2021-03-11 13:43:00 +0700

    View pada halaman

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,8 @@
+<nav class="tm-nav">
+              <ul>
+                <li><a href="{{ url('/') }}">home</a></li>
+                <li><a href="{{ url('/menu') }}">menu</a></li>
+                <li><a href="{{ url('/today-special') }}">today special</a></li>
+                <li><a href="{{ url('/contact') }}">contact</a></li>
+              </ul>
+</nav>   
