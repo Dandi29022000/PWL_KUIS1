9f847ae7225552eafb0b1115c4435f44ae604adc
= <=>
2021-03-11 14:13:06 +0700

    View

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,17 +1,8 @@
 <nav class="tm-nav">
-<<<<<<< HEAD
               <ul>
                 <li><a href="{{ url('/') }}">home</a></li>
                 <li><a href="{{ url('/menu') }}">menu</a></li>
                 <li><a href="{{ url('/today-special') }}">today special</a></li>
                 <li><a href="{{ url('/contact') }}">contact</a></li>
               </ul>
-=======
-    <ul>
-      <li><a href="{{ url('/') }}">home</a></li>
-      <li><a href="{{ url('/menu') }}">menu</a></li>
-      <li><a href="{{ url('/today-special') }}">today special</a></li>
-      <li><a href="{{ url('/contact') }}">contact</a></li>
-    </ul>
->>>>>>> bd8d0715fa592daa4d00add90ced277fce76ab57
 </nav>   
